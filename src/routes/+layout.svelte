350b5b83b1c5e19414ff53b009b58477bd9d4e7e
Adam Höllerl <a.hoellerl@students.htl-leonding.ac.at>
2023-12-07 19:31:35 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <h1>hoellerl</h1>
+    <a href="/">Home</a>
+    <a href="/about">About me</a>
+    <a href="/contact">Contact</a>
+    <a href="/login">Login</a>
+</nav>
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    let name = 'hoellerl';
+</script>
+
+<h1>{name}</h1>
+<p>Enter your name: </p>
+<input bind:value={name} placeholder="Enter your name" />
