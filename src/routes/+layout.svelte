5a97e64989cdf70a8984b065b1a6378205331465
Adam Höllerl <a.hoellerl@students.htl-leonding.ac.at>
2023-12-19 13:23:58 +0100

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -156,6 +156,10 @@
 <script lang="ts">
     import { locale, _} from 'svelte-i18n';
     import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    inject({ mode: dev ? 'development' : 'production' });
     injectSpeedInsights();
     const footerSep = "&nbsp;&nbsp;•&nbsp;&nbsp;";
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
