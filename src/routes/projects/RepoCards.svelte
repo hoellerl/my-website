8343964fb1ef3967705f73234cb787ab57c08095
Adam Höllerl <a.hoellerl@students.htl-leonding.ac.at>
2024-01-21 20:10:12 +0100

    increased timeout for seperator width

--- src/routes/projects/RepoCards.svelte
+++ src/routes/projects/RepoCards.svelte
@@ -124,7 +124,7 @@
         fetchRepos();
         setTimeout(() => {
             setSepWidth();
-        }, 100);
+        }, 200);
         window.addEventListener('resize', setSepWidth);
     });
 
